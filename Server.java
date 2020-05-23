3169f5488bbf47ca44df437b7d57df6e882a0379
Wuelle <58120269+Wuelle@users.noreply.github.com>
2020-05-23 21:24:56 +0200

    Added infinite rounds and server gui
    The server can only be shut down using the new JFrame. Whenever a round ends, a new one is immediately started, using the same mapsize and maxPlayers as before. The client does need to restart

--- Server.java
+++ Server.java
@@ -1,3 +1,7 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.DatagramPacket;
@@ -7,7 +11,11 @@ import java.net.SocketException;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Server implements Runnable{
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Server extends JPanel implements Runnable{
 	private DatagramSocket ServerSocket;
 	private String message;
 	private byte[] data;
@@ -22,6 +30,7 @@ public class Server implements Runnable{
 	public int[] player_ports;
 	public boolean[] player_dead;
 	public InetAddress[] player_ips;
+	private Thread thread;
 	
 	public int dir;
 	public final int EMPTY = 0;
@@ -35,8 +44,9 @@ public class Server implements Runnable{
 	public final int delayReduce = 100;
 	public final int minDelay = 100;
 	public int delay = startDelay;
+	public boolean gameOver = false;
 
-	//COmmandlline arguments
+	//Commandlline arguments
 	public static int mapsize;
 	public static int maxPlayers;
 	
@@ -46,7 +56,6 @@ public class Server implements Runnable{
 	public static int[][] grid;
 	
 	public Server() {
-		//Jeder Spieler verbindet sich zuerst mit dem loginport und bekommt dann einen individuellen zugewiesen
 		try {
 			this.ServerSocket = new DatagramSocket(2000);
 		}catch(BindException e) {
@@ -155,95 +164,108 @@ public class Server implements Runnable{
 
 
 	public void run() {
-		resetMap();
-		//Wait for enough players to arrive(Nr specified in args)
-		for(int x = 0; x < maxPlayers; x++) {
-			System.out.println("Waiting for " + String.valueOf(maxPlayers-players) + " more Players...");
-			byte[] data = new byte[1024];
-			LoginPacket = new DatagramPacket(data, data.length);
-			try {
-				ServerSocket.receive(LoginPacket);
-				message = new String(LoginPacket.getData()).trim();
-				System.out.println(message + " just logged in from " + LoginPacket.getAddress().toString() + "[" + String.valueOf(LoginPacket.getPort()) + "]");
+		while (true) {
+			//Reset the whole Gamestate
+			players = 0;
+			gameOver = false;
+			player_usernames = new String[maxPlayers];
+			player_ports = new int[maxPlayers];
+			player_ips = new InetAddress[maxPlayers];;
+			player_dead = new boolean[maxPlayers];
+			delay = startDelay;
+			Arrays.fill(player_dead, false);
+			resetMap();
+			
+			//Wait for enough players to arrive(Nr specified in args)
+			for(int x = 0; x < maxPlayers; x++) {
+				System.out.println("Waiting for " + String.valueOf(maxPlayers-players) + " more Players...");
+				byte[] data = new byte[1024];
+				LoginPacket = new DatagramPacket(data, data.length);
+				try {
+					ServerSocket.receive(LoginPacket);
+					message = new String(LoginPacket.getData()).trim();
+					System.out.println(message + " just logged in from " + LoginPacket.getAddress().toString() + "[" + String.valueOf(LoginPacket.getPort()) + "]");
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				
-			} catch (IOException e) {
-				e.printStackTrace();
+				sendPacket((message + ":" + String.valueOf(mapsize) + ":" + String.valueOf(maxPlayers)).getBytes(), LoginPacket.getAddress(), LoginPacket.getPort(), ServerSocket);
+				players += 1;
+
+				player_ips[x] = LoginPacket.getAddress();
+				player_ports[x] = LoginPacket.getPort();
+				player_usernames[x] = message;
 			}
 			
-			sendPacket((message + ":" + String.valueOf(mapsize) + ":" + String.valueOf(maxPlayers)).getBytes(), LoginPacket.getAddress(), LoginPacket.getPort(), ServerSocket);
-			players += 1;
-
-			player_ips[x] = LoginPacket.getAddress();
-			player_ports[x] = LoginPacket.getPort();
-			player_usernames[x] = message;
-		}
-		
-		//Start the actual game
-		System.out.println("All " + String.valueOf(maxPlayers) + " Players entered, may the best survive!");
-		
-		player_positions = pickStartPositions(maxPlayers);
-		while(isRunning){
-			data = getResponseData();
+			//Start the actual game
+			System.out.println("All " + String.valueOf(maxPlayers) + " Players entered, may the best survive!");
 			
-			for (int x = 0; x < maxPlayers; x++) {
-				sendPacket(data, player_ips[x], player_ports[x], ServerSocket);
-				message = waitForResponse(ServerSocket);
-				String[] chunks = message.split(":");
-				if(!player_dead[x]) {
-					//test if the response is actually from the expected player
-					if(chunks[0].equals(player_usernames[x])) {
-						
-						dir = Integer.parseInt(chunks[1]);
-						if (dir == UP) {
-							player_positions[x][1] -= 1;
-						}
-						else if (dir == DOWN) {
-							player_positions[x][1] += 1;
-						}
-						else if (dir == LEFT) {
-							player_positions[x][0] -= 1;
-						}
-						else if (dir == RIGHT) {
-							player_positions[x][0] += 1;
-						}
-						
-						//update the serverside gamestate
-						int c = grid[player_positions[x][0]][player_positions[x][1]];
-						System.out.println("Player is standing on " + String.valueOf(c));
-						
-						if (c == EMPTY) {
-							grid[player_positions[x][0]][player_positions[x][1]] = x+1;
-						}
-						else {
-							player_dead[x] = true;
+			player_positions = pickStartPositions(maxPlayers);
+			while(!gameOver){
+				data = getResponseData();
+				
+				for (int x = 0; x < maxPlayers; x++) {
+					sendPacket(data, player_ips[x], player_ports[x], ServerSocket);
+					message = waitForResponse(ServerSocket);
+					String[] chunks = message.split(":");
+					if(!player_dead[x]) {
+						//test if the response is actually from the expected player
+						if(chunks[0].equals(player_usernames[x])) {
 							
-							PurgeMap(x+1);
+							dir = Integer.parseInt(chunks[1]);
+							if (dir == UP) {
+								player_positions[x][1] -= 1;
+							}
+							else if (dir == DOWN) {
+								player_positions[x][1] += 1;
+							}
+							else if (dir == LEFT) {
+								player_positions[x][0] -= 1;
+							}
+							else if (dir == RIGHT) {
+								player_positions[x][0] += 1;
+							}
 							
-							System.out.println(player_usernames[x] + " just died!");
-							if(everyoneDead(player_dead)) {
-								isRunning = false;
+							//update the serverside gamestate
+							int c = grid[player_positions[x][0]][player_positions[x][1]];
+							System.out.println("Player is standing on " + String.valueOf(c));
+							
+							if (c == EMPTY) {
+								grid[player_positions[x][0]][player_positions[x][1]] = x+1;
+							}
+							else {
+								player_dead[x] = true;
+								
+								PurgeMap(x+1);
+								
+								System.out.println(player_usernames[x] + " just died!");
+								if(everyoneDead(player_dead)) {
+									gameOver = true;
+								}
+								
 							}
 							
 						}
-						
+						else {
+							System.out.println("Received packet from " + chunks[0] + " but expected " + player_usernames[x]);
+						}
 					}
-					else {
-						System.out.println("Received packet from " + chunks[0] + " but expected " + player_usernames[x]);
-					}
+					
 				}
 				
-			}
-			
-			try {
-				//the game starts off slowly but speeds up over time
-				Thread.sleep(delay);
-				if(delay > minDelay) {
-					delay -= delayReduce;
+				try {
+					//the game starts off slowly but speeds up over time
+					Thread.sleep(delay);
+					if(delay > minDelay) {
+						delay -= delayReduce;
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
 		}
+		
 		
 	}
 	
@@ -252,25 +274,46 @@ public class Server implements Runnable{
 			return;
 		System.out.println("Start");
 		isRunning = true;
-		
-		player_usernames = new String[maxPlayers];
-		player_ports = new int[maxPlayers];
-		player_ips = new InetAddress[maxPlayers];;
-		player_dead = new boolean[maxPlayers];
 		grid = new int[mapsize][mapsize];
 		
-		Arrays.fill(player_dead, false);
+		JFrame frame = new JFrame();
+		JButton btn_shutdown = new JButton("Stop Server");
+		frame.add(btn_shutdown);
+		btn_shutdown.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e)
+		    {
+		    	System.out.println("Manually stopped server");
+		    	frame.dispose();
+		    	System.exit(1);
+
+		    }
+		});
+		frame.setResizable(false);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null); // Frame zentrieren
+		frame.setVisible(true);
+		frame.setFocusable(true);
+		frame.setTitle("Server");
+		
+		Dimension dimension = new Dimension(100, 100);
+		frame.setPreferredSize(dimension);
+		frame.setMinimumSize(dimension);
+		frame.setMaximumSize(dimension);
+		
+		
+		
+		frame.validate();
 		
 		//Spiel in neuem Thread starten
-		Thread thread = new Thread(this);
+		thread = new Thread(this);
 		thread.start();
 		
 		
 		
 	}
 	
-	public static void main(String[] args) {
-			
+	public static void main(String[] args) {	
 		Server server = new Server();
 		
 		//Commandline arguments
